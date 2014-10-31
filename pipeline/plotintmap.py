2fdbc505963726cc79580f98df9eb1a2cd422f94
Steven Mohammed <smohammed@astro.columbia.edu>
2014-10-31 15:41:52 -0400

    Added maps of different regions

--- pipeline/plotintmap.py
+++ pipeline/plotintmap.py
@@ -1,7 +1,7 @@
 from astropy.io import fits
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
-
+import numpy as np
 
 def plotmap(a,b):
 	a=a		# Start gl
@@ -9,19 +9,12 @@ def plotmap(a,b):
 	
 	pixscale = 1 * (b - 20.)/20.
 	
-	phomap = fits.open('../intensitymap1200'+'_'+str(a)+'_'+str(b)+'.fits')[0].data.T
+	phomap = np.sqrt(fits.open('../intensitymap1200'+'_'+str(a)+'_'+str(b)+'.fits')[0].data).T
 	expmap = fits.open('../8-28-aspcorr_new_scst.fits')[0].data.T
-	
-	#xexpmap,yexpmap = expmap.shape[0],expmap.shape[1]
-	#expmap = expmap[]
 	
 	expmap1 = expmap[39:1238,39+1199*pixscale:1238+1199*pixscale]
 	
 	intmap = phomap/expmap1
 	
-	return plt.imshow(intmap,vmin=0,vmax=10,origin='lower',extent=[a,b,-10,10],interpolation='nearest',aspect='auto',cmap=cm.gray), plt.show()	
-
-#a,b,intmap = plotmap(a,b)
-
-
+	return plt.imshow(intmap,vmin=0,vmax=0.4,origin='lower',extent=[a,b,-10,10],interpolation='nearest',aspect='auto',cmap=cm.gray), plt.show()	
 
