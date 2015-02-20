049fbe025040bf18523b3e62015d0e59fcab0016
Steven Mohammed <smohammed@astro.columbia.edu>
2015-02-20 13:15:13 -0500

    Modified sourcefinder, added phot counts & stats

--- pipeline/sourcefinder.py
+++ pipeline/sourcefinder.py
@@ -11,8 +11,8 @@ import matplotlib.gridspec as gridspec
 ##############################################################
 ##############################################################
 # TO DO
-# 2/10
-# - Stack brightest stars, look for dx dy offset
+# 2/19
+# - Reset dx,dy on star peak, take some radius from that, compute mean, std
 ##############################################################
 ##############################################################
 
@@ -40,20 +40,21 @@ field = fits.open('../corrcsv/00005_0001galcorr.csv.fits')[1].data
 ##############################################################
 names,times,nphotons,stargl,stargb,phogl,phogb = [],[],[],[],[],[],[]
 timestep = np.arange(np.round(field.time[-1])+1)
-tstep = 210
+tstep = 0
 maxtime = 0
-
-#fig = plt.figure(figsize=(8,8))
-
-#grid = gridspec.GridSpec(5,10,wspace=0.0,hspace=0.0)
-gridcount = 0
 expsize = 5.
-arcminlim = 1.
-totdx,totdy = [],[]
+arcminlim = 2.
+totdx,totdy,totphot2, totphot15,stdsumdx,stdsumdy = [],[],[],[],[],[]
+timesteplimit = -1 	#Index of the timestep limits. -1 is the entire set
 
-# Limit to the first three timesteps to make it run faster
-while tstep < timestep[-1]:
-	print tstep
+plotgrid = 0
+if plotgrid == 1:
+	fig = plt.figure(figsize=(8,8))
+	grid = gridspec.GridSpec(5,10,wspace=0.0,hspace=0.0)
+	gridcount = 0
+
+while (tstep < timestep[timesteplimit]) and (tstep != 1340.):
+	print 'tstep = ', tstep
 	tstep += expsize
 
 	fieldlim = np.where((field.time >= tstep) & (field.time < tstep+expsize))
@@ -61,10 +62,9 @@ while tstep < timestep[-1]:
 	fgb = field.gb[fieldlim]
 
 	# Fix neg values of fgl that are ~360 deg to neg numbers to keep circular shape
-	for i in range(len(fgl)):
-		if min(fgl) < 10. and fgl[i] > 310.:
-			fgl[i] = fgl[i] - 360.
-	
+	if len(fgl) > 0.:
+		fgl[(min(fgl) < 10.) & (fgl > 350.)] = fgl[(min(fgl) < 10.) & (fgl > 350.)] - 360.
+
 	# Convert to galactic coordinates and limit the bstar range 
 	fgal = SkyCoord(fgl.tolist()*u.degree, fgb.tolist()*u.degree, frame='galactic')
 	bcut2 = np.where((bgal.l.degree > np.min(fgl)) & (bgal.l.degree < np.max(fgl)) & (bgal.b.degree > -10.) & (bgal.b.degree < 10.))
@@ -75,219 +75,203 @@ while tstep < timestep[-1]:
 	# Find photons within 2' of each star 
 	##############################################################	
 	starind, photind, starsep, stardist = search_around_sky(bgal2,fgal,arcminlim*u.arcmin)
-	bstar2 = bstar2[np.unique(starind)]
-	starname = bstar2.galex_id
-	bgal2 = SkyCoord(bstar2.ra*u.degree, bstar2.dec*u.degree, frame='icrs').galactic
-	bgl = bgal2.l.degree
-	bgb = bgal2.b.degree
-
+	bstar2am = bstar2[np.unique(starind)]
+	starname = bstar2am.galex_id
+	bgal2am = SkyCoord(bstar2am.ra*u.degree, bstar2am.dec*u.degree, frame='icrs').galactic
+	bgl = bgal2am.l.degree
+	bgb = bgal2am.b.degree
 	fgl = fgal.l.degree
 	fgb = fgal.b.degree
 	
-	# Fix neg values of fgl that are ~360 deg to neg numbers to keep circular shape
-	for i in range(len(fgl)):
-		if min(fgl) < 10. and fgl[i] > 310.:
-			fgl[i] = fgl[i] - 360.
-	
+	# Fix neg values of fgl again so that are ~360 deg to neg numbers to keep circular shape
+	fgl[(min(fgl) < 10.) & (fgl > 350.)] = fgl[(min(fgl) < 10.) & (fgl > 350.)] - 360.
+
 	# Make a list for each star/photon list pair...probably more efficient way to do this
 	starnumber = []
 	for i in range(len(starind)-1):
 		if starind[i] != starind[i+1]:
 			starnumber.append(i) 		# Do this so we can match indices with photind
 
-	print starnumber
-
-	pllist = []
-	pblist = []
-	pllist.append(fgl[photind[0:starnumber[0]+1]])
-	pblist.append(fgb[photind[0:starnumber[0]+1]])
-	for i in range(len(starnumber)-1):
-		pllist.append(fgl[photind[starnumber[i]+1:starnumber[i+1]+1]])
-		pblist.append(fgb[photind[starnumber[i]+1:starnumber[i+1]+1]])
-	pllist.append(fgl[photind[starnumber[-1]+1:-1]])
-	pblist.append(fgb[photind[starnumber[-1]+1:-1]])
-
-	maxnuv = np.argsort(bstar2.nuv_cps[-10:])[::-1] 
-	print maxnuv
-
-	##############################################################
-	# Get offset for each point 
-	##############################################################
-	# First translate each star coords to (0,0)
-	dx,dy,sumdx,sumdy = [],[],[],[]
-	for i in maxnuv:
-		dx.append(bgl[i] - pllist[i])
-		dy.append(bgb[i] - pblist[i])
-
-	for i in range(len(dx)):
-		sumdx = np.concatenate((sumdx,dx[i].tolist()))
-		sumdy = np.concatenate((sumdy,dy[i].tolist()))
+	if len(starnumber) > 0.:
+		pllist = []
+		pblist = []
+		pllist.append(fgl[photind[0:starnumber[0]+1]])
+		pblist.append(fgb[photind[0:starnumber[0]+1]])
+		for i in range(len(starnumber)-1):
+			pllist.append(fgl[photind[starnumber[i]+1:starnumber[i+1]+1]])
+			pblist.append(fgb[photind[starnumber[i]+1:starnumber[i+1]+1]])
+		pllist.append(fgl[photind[starnumber[-1]+1:-1]])
+		pblist.append(fgb[photind[starnumber[-1]+1:-1]])
 	
-	totdx.append(np.mean(sumdx))
-	totdy.append(np.mean(sumdy))
-
-	##############################################################
-	# Plot histograms!
-	##############################################################
-
-	fig = plt.figure(figsize=(8,8))
-	plt.hist(sumdx,bins=50)
-	plt.savefig('tstep-'+str(tstep)+'_dx_mean-'+str(np.mean(sumdx))[:8]+'.png')
-	plt.close()
+		maxnuv = np.argsort(bstar2am.nuv_cps[-10:])[::-1]
+		print 'maxnuv =', maxnuv
 	
-	fig = plt.figure(figsize=(8,8))
-	plt.hist(sumdy,bins=50)
-	plt.savefig('tstep-'+str(tstep)+'_dy_mean-'+str(np.mean(sumdy))[:8]+'.png')
-	plt.close()
-
-	'''
-	plotgrid = 0
-	if plotgrid == 1:
-		for i in range(len(maxnuv)):
-			ax = plt.Subplot(fig,grid[gridcount])
-			ax.scatter(bgl[maxnuv[i]],bgb[maxnuv[i]],s=100,facecolor='none',edgecolor='blue',	linewidth='2')
-			ax.scatter(pllist[maxnuv[i]],pblist[maxnuv[i]],marker='.',s=1)
-			ax.set_xticks([])
-			ax.set_yticks([])
-			axislim = 0.1 
-			ax.text(bgl[maxnuv[i]]-axislim+0.01,bgb[maxnuv[i]]-axislim,str(starname[maxnuv[i]]))
-			ax.set_xlim(bgl[maxnuv[i]]-axislim,bgl[maxnuv[i]]+axislim)
-			ax.set_ylim(bgb[maxnuv[i]]-axislim,bgb[maxnuv[i]]+axislim)
-			fig.add_subplot(ax)
-			gridcount+=1
-	'''
-
-	otherstuff = 0
-	if otherstuff == 1:
-		'''
-		bskyfix = 0
-		if bskyfix == 1: # Do the same gl fix as for the fgl values
-			for i in range(len(bgl)):
-				if min(bgl) < 10. and bgl[i] > 310.:
-					bgl[i] = bgl[i] - 360.
+		##############################################################
+		# Get offset for each point, recenter, calc mean, stdev
+		##############################################################
+		# First translate each star coords to (0,0)
+		dx,dy,sumdx,sumdy = [],[],[],[]
+		for i in maxnuv:
+			dx.append((bgl[i] - pllist[i])*60.)
+			dy.append((bgb[i] - pblist[i])*60.)
+	
+		for i in range(len(dx)):
+			sumdx = np.concatenate((sumdx,dx[i].tolist()))
+			sumdy = np.concatenate((sumdy,dy[i].tolist()))
 		
-		# Check to see that you're not totally screwing up by plotting
-		plot = 0
-		if plot == 1:
-			plt.scatter(bgl,bgb,c='green',s=20,facecolor='none')
-			plt.scatter(fgl[photind],fgb[photind],c='orange',s=10,edgecolor='none')
-			#plt.scatter(fgl,fgb,c='red',s=3,edgecolor='none')
-			plt.show()
-			break
+		totphot2.append(len(sumdx))
+		
+		#newbcenter = SkyCoord(np.zeros(len(sumdx))*u.degree, np.zeros(len(sumdx))*u.degree, frame='galactic')
+		#newphotcenter = SkyCoord(sumdx*u.degree, sumdy*u.degree, frame='galactic')
+
+		#starind1am, photind1am, starsep1am, stardist1am = search_around_sky(newbcenter,newphotcenter,1.*u.arcmin)
+
+		totdx.append(np.mean(sumdx))
+		totdy.append(np.mean(sumdy))
+		stdsumdx.append(np.std(sumdx))
+		stdsumdy.append(np.std(sumdy))
+
+		##############################################################
+		# Make plots
+		##############################################################
+		plot_hist = 0.
+		if plot_hist == 1:
+			fig = plt.figure(figsize=(8,8))
+			plt.hist(sumdx,bins=50)
+			plt.savefig('tstep-'+str(tstep)+'_dx_mean-'+str(np.mean(sumdx))[:8]+'.png')
+			plt.close()
+		
+			fig = plt.figure(figsize=(8,8))
+			plt.hist(sumdy,bins=50)
+			plt.savefig('tstep-'+str(tstep)+'_dy_mean-'+str(np.mean(sumdy))[:8]+'.png')
+			plt.close()
+
+		if plotgrid == 1:
+			for i in range(len(maxnuv)):
+				ax = plt.Subplot(fig,grid[gridcount])
+				ax.scatter(bgl[maxnuv[i]],bgb[maxnuv[i]],s=100,facecolor='none',edgecolor='blue',		linewidth='2')
+				ax.scatter(pllist[maxnuv[i]],pblist[maxnuv[i]],marker='.',s=1)
+				ax.set_xticks([])
+				ax.set_yticks([])
+				axislim = 0.1 
+				ax.text(bgl[maxnuv[i]]-axislim+0.01,bgb[maxnuv[i]]-axislim,str(starname[maxnuv[i]])	)
+				ax.set_xlim(bgl[maxnuv[i]]-axislim,bgl[maxnuv[i]]+axislim)
+				ax.set_ylim(bgb[maxnuv[i]]-axislim,bgb[maxnuv[i]]+axislim)
+				fig.add_subplot(ax)
+				gridcount+=1
 	
-		# Add photon times for each index
-		photon = []
-		for i in starind:
-			photon.append(sum(starind == i))
+	else:
+		print 'no stars in slice tstep =', tstep
+		totdx.append(0.)
+		totdy.append(0.)
+		stdsumdx.append(0.)
+		stdsumdy.append(0.)
+		totphot2.append(0.)
+
+
+	find_Nphot15 = 0.
+	if find_Nphot15 == 1:
+		##############################################################
+		# Find photons in a 15' radius now
+		##############################################################
+		starind15, photind15, starsep15, stardist15 = search_around_sky(bgal2,fgal,15.*u.arcmin)
+		bstar215 = bstar2[np.unique(starind15)]
+		starname15 = bstar215.galex_id
+		bgal215 = SkyCoord(bstar215.ra*u.degree, bstar215.dec*u.degree, frame='icrs').galactic
+		bgl15 = bgal215.l.degree
+		bgb15 = bgal215.b.degree
 	
-		#Add values to arrays so you can then make a big table
-		cutnames = np.unique(starname)
-		bgl = np.unique(bgl)
-		bgb = np.unique(bgb)
-		fieldgl = fgl[photind].tolist()
-		fieldgb = fgb[photind].tolist()
+		fgl15 = fgal.l.degree
+		fgb15 = fgal.b.degree
+		
+		# Fix neg values of fgl that are ~360 deg to neg numbers to keep circular shape
+		fgl15[(min(fgl15) < 10.) & (fgl15 > 350.)] = fgl15[(min(fgl15) < 10.) & (fgl15 > 350.)] - 	360.
 	
-		names = np.concatenate((names,starname),axis=0)
-		stargl = np.concatenate((stargl,bgl),axis=0)
-		stargb = np.concatenate((stargb,bgb),axis=0)
-		phogl.append(fieldgl)
-		phogb.append(fieldgb)
-		times = np.concatenate((times,np.ones(len(names))*tstep),axis=0)
-		nphotons = np.concatenate((nphotons,photon),axis=0)
-		'''
+		# Make a list for each star/photon list pair...probably more efficient way to do this
+		starnumber15 = []
+		for i in range(len(starind15)-1):
+			if starind15[i] != starind15[i+1]:
+				starnumber15.append(i) 		# Do this so we can match indices with photind
+	
+		#print 'starnumber =', starnumber
+	
+		if len(starnumber15) > 0.:
+			pllist15 = []
+			pblist15 = []
+			pllist15.append(fgl15[photind15[0:starnumber15[0]+1]])
+			pblist15.append(fgb15[photind15[0:starnumber15[0]+1]])
+			for i in range(len(starnumber15)-1):
+				pllist15.append(fgl15[photind15[starnumber15[i]+1:starnumber15[i+1]+1]])
+				pblist15.append(fgb15[photind15[starnumber15[i]+1:starnumber15[i+1]+1]])
+			pllist15.append(fgl15[photind15[starnumber15[-1]+1:-1]])
+			pblist15.append(fgb15[photind15[starnumber15[-1]+1:-1]])
+		
+			maxnuv15 = np.argsort(bstar215.nuv_cps[-10:])[::-1] 
+			print 'maxnuv15 =', maxnuv15
+		
+			##############################################################
+			# Get offset for each point, recenter, calc mean, stdev
+			##############################################################
+			# First translate each star coords to (0,0)
+			dx15,dy15,sumdx15,sumdy15 = [],[],[],[]
+			for i in maxnuv:
+				dx15.append((bgl15[i] - pllist15[i])*60.)
+		
+			for i in range(len(dx15)):
+				sumdx15 = np.concatenate((sumdx15,dx15[i].tolist()))
+			
+			totphot15.append(len(sumdx15))
 
+totdx.append(0.)
+totdy.append(0.)
+stdsumdx.append(0.)
+stdsumdy.append(0.)
+totphot2.append(0.)
 
-print totdx
-print ' '
-print totdy
+print 'totdx =', len(totdx)
+print 'totdy =', len(totdy)
+print 'totphot2 =', len(totphot2)
 
-fig = plt.figure(figsize=(8,8))
-plt.scatter(np.arange(0,timestep[-1],5),totdx)
-plt.savefig('dxvstime.png')
-
-
-fig = plt.figure(figsize=(8,8))
-plt.scatter(np.arange(0,timestep[-1],5),totdy)
-plt.savefig('dyvstime.png')
-
-
-
-a1 = np.array(np.arange(0,timestep[-1],5))
-a2 = np.array(totdx)
-a3 = np.array(totdy)
-cols = []
-col1 = fits.Column(name='time', format='E', array=a1)
-col2 = fits.Column(name='dx', format='E', array=a2)
-col3 = fits.Column(name='dy', format='E', array=a3)
-cols.append(col1)
-cols.append(col2)
-cols.append(col3)
-new_cols = fits.ColDefs(cols)
-hdu = fits.BinTableHDU.from_columns(new_cols)
-hdu.writeto('staroffset.fits')
-
-
-'''
-a = []
-b = []
-for i in range(len(dx)):
-	for j in range(len(dx[i])):
-		if (np.sqrt(dx[i][j]**2 + dy[i][j]**2) < max(np.abs(dx[i]))/2.):
-			a.append(j)
-	b.append(a)
-	a = []
-
-
-for i in range(len(dx)):
-	plt.scatter(dx[i],dy[i],s=1,alpha=0.5)
-
-#plt.title('Stacked photons 0-50s')
-
-
-#plt.xlabel('10 Brightest stars, left to right')
-#plt.ylabel('time, top to bottom')
-
-#plt.subplots_adjust(hspace=None,wspace=None)
-
-
-
-plt.show()
-#plt.savefig('bstar_lim_'+str(arcminlim)+'_exp_'+str(expsize)+'.png')
-'''
-##############################################################
-# Now find average photon displacement from each star
-##############################################################
-
+f, axarr = plt.subplots(3, sharex=True,figsize=(16,16))
+axarr[0].errorbar(np.arange(0,timestep[timesteplimit],5)-1,totdx,yerr=stdsumdx,fmt='o')
+axarr[0].set_ylabel('dx [arcmin]')
+axarr[0].set_xlim(0,timestep[timesteplimit])
+axarr[1].errorbar(np.arange(0,timestep[timesteplimit],5)-1,totdy,yerr=stdsumdy,fmt='o')
+axarr[1].set_ylabel('dy [arcmin]')
+axarr[2].scatter(np.arange(0,timestep[timesteplimit],5)-1, totphot2)
+axarr[2].set_xlabel('Time [s]')
+axarr[2].set_ylabel('NPhot 2arcmin')
+axarr[2].axhline(y=100)
+f.subplots_adjust(hspace=0)
+f.savefig('../testplots.png')
 
 ##############################################################
-# Make the table
+# Make a table of dx, dy, Nphotons in 2' and 15' 
 ##############################################################
-make_table = 0
-if make_table == 1:
-	a1 = np.array(names)
-	a2 = np.array(stargl)
-	a3 = np.array(stargb)
-	a4 = np.array(phogl)
-	a5 = np.array(phogb)
-	a6 = np.array(times)
-	a7 = np.array(nphotons)
+make_star_table = 0
+if make_star_table == 1.:
+	a1 = np.array(np.arange(0,timestep[timesteplimit],5))
+	a2 = np.array(totdx)
+	a3 = np.array(totdy)
+	a4 = np.array(totphot2)
+	a5 = np.array(totphot15)
 	cols = []
-	col1 = fits.Column(name='name', format='E', array=a1)
-	col2 = fits.Column(name='star_gl', format='E', array=a2)
-	col3 = fits.Column(name='star_gb', format='E', array=a3)
-	col4 = fits.Column(name='photon_gl', format='20E', array=a4)
-	col5 = fits.Column(name='photon_gb', format='E', array=a5)
-	col6 = fits.Column(name='times', format='E', array=a6)
-	col7 = fits.Column(name='nphotons', format='E', array=a7)
+	col1 = fits.Column(name='time', format='E', array=a1)
+	col2 = fits.Column(name='dx', format='E', array=a2)
+	col3 = fits.Column(name='dy', format='E', array=a3)
+	col4 = fits.Column(name='Nphot2', format='E', array=a4)
+	col5 = fits.Column(name='Nphot15', format='E', array=a5)	
 	cols.append(col1)
 	cols.append(col2)
 	cols.append(col3)
 	cols.append(col4)
 	cols.append(col5)
-	cols.append(col6)
-	cols.append(col7)
 	new_cols = fits.ColDefs(cols)
 	hdu = fits.BinTableHDU.from_columns(new_cols)
-	hdu.writeto('00005_0001galcorr_bstarphotons.fits')
+	hdu.writeto('staroffset.fits')
+
+##############################################################
+# Now find average photon displacement from each star
+##############################################################
 
 
