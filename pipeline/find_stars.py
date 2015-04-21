b34254b394f7e326412d0db0bc33d3cfe6e64bd7
Steven Mohammed <smohammed@astro.columbia.edu>
2015-04-21 15:33:19 -0400

    Added find data to find_stars table output

--- pipeline/find_stars.py
+++ pipeline/find_stars.py
@@ -30,11 +30,11 @@ s_cgal = SkyCoord(sgl*u.degree, sgb*u.degree, frame='galactic')
 ###################################################################
 # Load bstars
 ###################################################################
-bstar = fits.open('../bstar2.fits')[1].data
+bstar = fits.open('../tychobstarmatch.fits')[1].data
 bcut1 = np.where((bstar.nuv_cps > 10.) & (bstar.nuv_cps < 100.))
-bstar = bstar[bcut1]
-bcut2 = np.where((bstar.gl > 0) & (bstar.gl < 2) & (bstar.gb > -2.) & (bstar.gb < 0.))
-bstar2 = bstar[bcut2]
+bstar1 = bstar[bcut1]
+bcut2 = np.where((bstar1.gl > 0) & (bstar1.gl < 2) & (bstar1.gb > -2.) & (bstar1.gb < 0.))
+bstar2 = bstar1[bcut2]
 bgal2 = SkyCoord(bstar2.gl*u.degree, bstar2.gb*u.degree, frame='galactic')
 
 ###################################################################
@@ -43,41 +43,60 @@ bgal2 = SkyCoord(bstar2.gl*u.degree, bstar2.gb*u.degree, frame='galactic')
 bstarind, star_candind, starangsep, stardist3d = search_around_sky(bgal2, s_cgal, 1.*u.arcmin)
 
 uniqueind = np.unique(bstarind, return_index=True)[1]
+anglim = np.where(starangsep.degree[uniqueind] < 0.002)
 
 bstar3 = bstar2[bstarind[uniqueind]]
-bstarname = bstar3.galex_id
-bgal3 = SkyCoord(bstar3.ra*u.degree, bstar3.dec*u.degree, frame='icrs').galactic
-bgl = bgal3.l.degree
-bgb = bgal3.b.degree
-bflux = bgal3.nuv_cp
-
 s_match = star_cand[star_candind[uniqueind]]
+
+bstar3 = bstar3[anglim]
+bstarname = bstar3.galex_id
+bgl = bstar3.gl
+bgb = bstar3.gb
+bflux = bstar3.nuv_cps
+
+s_match = s_match[anglim]
 sgl = s_match.gl
 sgb = s_match.gb
 sflux = s_match.flux
 
-a1 = bstar3.galex_id
-a2 = bstar3.cat_id
-a3 = bstar3.ra
-a4 = bstar3.dec
-a5 = bgl
-a6 = bgb
-a7 = bstar3.fuv_cps
-a8 = bstar3.fuv_cat
-a9 = bstar3.nuv_cps
-a10 = bstar3.nuv_cat
-
-col1 = fits.Column(name='galex_id', format='E', array=a1)
-col2 = fits.Column(name='cat_id', format='10A', array=a2)
-col3 = fits.Column(name='ra', format='E', array=a3)
-col4 = fits.Column(name='dec', format='E', array=a4)
-col5 = fits.Column(name='gl', format='E', array=a5)
-col6 = fits.Column(name='gb', format='E', array=a6)
-col7 = fits.Column(name='fuv_cps', format='E', array=a7)
-col8 = fits.Column(name='fuv_cat', format='10A', array=a8)
-col9 = fits.Column(name='nuv_cps', format='E', array=a9)
-col10 = fits.Column(name='nuv_cat', format='10A', array=a10)
-
-new_cols = fits.ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10])
-hdu = fits.BinTableHDU.from_columns(new_cols)
-#hdu.writeto('../find_bstar_matches.fits')
+# Make a table
+table = 0
+if table == 1:
+	a1 = bstar3.galex_id
+	a2 = bstar3.cat_id
+	a3 = bstar3.ra
+	a4 = bstar3.dec
+	a5 = bgl
+	a6 = bgb
+	a7 = bstar3.fuv_cps
+	a8 = bstar3.fuv_cat
+	a9 = bstar3.nuv_cps
+	a10 = bstar3.nuv_cat
+	a11 = bstar3.BTmag
+	a12 = bstar3.VTmag
+	a13 = bstar3.VTmag - 0.090 * (bstar3.BTmag-bstar3.VTmag)
+	a14 = a13 + 0.850 * (bstar3.BTmag-bstar3.VTmag)
+	a15 = s_match.gl
+	a16 = s_match.gb
+	a17 = sflux
+	col1 = fits.Column(name='galex_id', format='E', array=a1)
+	col2 = fits.Column(name='cat_id', format='10A', array=a2)
+	col3 = fits.Column(name='ra', format='E', array=a3)
+	col4 = fits.Column(name='dec', format='E', array=a4)
+	col5 = fits.Column(name='gl', format='E', array=a5)
+	col6 = fits.Column(name='gb', format='E', array=a6)
+	col7 = fits.Column(name='fuv_cps', format='E', array=a7)
+	col8 = fits.Column(name='fuv_cat', format='10A', array=a8)
+	col9 = fits.Column(name='nuv_cps', format='E', array=a9)
+	col10 = fits.Column(name='nuv_cat', format='10A', array=a10)
+	col11 = fits.Column(name='BTmag', format='E', array=a11)
+	col12 = fits.Column(name='VTmag', format='E', array=a12)
+	col14 = fits.Column(name='Bmag', format='E', array=a14)
+	col13 = fits.Column(name='Vmag', format='E', array=a13)
+	col15 = fits.Column(name='find_gl', format='E', array=a15)
+	col16 = fits.Column(name='find_gb', format='E', array=a16)
+	col17 = fits.Column(name='find_flux', format='E', array=a17)
+	new_cols = fits.ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14,col15,col16,col17])
+	hdu = fits.BinTableHDU.from_columns(new_cols)
+	hdu.writeto('../find_bstar_t2_matches.fits')
+	
