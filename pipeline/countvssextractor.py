8adb0d697a3d9264e99ace17534fa6377925d830
Steven Mohammed <smohammed@astro.columbia.edu>
2015-07-24 17:13:04 -0400

    Added to countsvsmatches

--- pipeline/countvssextractor.py
+++ pipeline/countvssextractor.py
@@ -10,3 +10,16 @@ matplotlib.rcParams['font.size'] = 20
 
 # Compare sextractor number counts and catalog match counts with various plots
 
+f1counts = Table.read('../newfield.sex_total_05-68.txt',format='ascii')
+f1matches = Table.read('../starcatalog_05-68_2mass_t2.txt',format='ascii')
+
+print 'f1counts = ', len(f1counts)
+print 'f1matches = ', len(f1matches)
+
+
+
+h1 = plt.hist(f1counts['nuv'],bins=100,color='red',label='counts')
+h2 = plt.hist(f1matches['nuv'],bins=100,color='blue',label='matches')
+plt.title('New field Sextractor counts vs catalog matches')
+plt.legend(loc=2)
+
