a99d365c37972338f85b083ed8d31b003518d6a9
Steven Mohammed <smohammed@astro.columbia.edu>
2015-07-28 13:00:09 -0400

    Added new plots

--- pipeline/misc.py
+++ pipeline/misc.py
@@ -11,45 +11,50 @@ for i in picklefiles:
     k.append(a['col2'][4089])
 
 star = fits.open('starcatalog_120_rand_deadtime.fits')[1].data
-newt = Table.read('galex120_2mass_t2.txt',format='ascii')
-pickles = Table.read('picklemags.txt',format='ascii')
+
+star = Table.read('starcatalog_05-68_2mass_t2.txt', format='ascii')
+newt = Table.read('galex120_2mass_t2.txt', format='ascii')
+pickles = Table.read('picklemags.txt', format='ascii')
 
 i = 120
 gbrange = 5.
-scut = np.where((np.abs(star.gb) > gbrange))
-scut2 = np.where((np.abs(star.gb) < gbrange))
+scut = np.where((np.abs(star['gb']) > gbrange))
+scut2 = np.where((np.abs(star['gb']) < gbrange))
 ncut = np.where(np.abs(newt['gb']) > gbrange)
 ncut2 = np.where(np.abs(newt['gb']) < gbrange)
+
 f, (ax1, ax2) = plt.subplots(1, 2)
 plt.rc('legend',**{'fontsize':15})
-a1 = ax1.scatter(star.j[scut]-star.k[scut],star.nuv[scut]-star.j[scut],edgecolor='none',alpha=0.5)
-ax1.set_title('gb > '+str(gbrange)+', gl = '+str(i)+' to '+str(i+20))
-ax1.set_xlim((-0.5,1.5))
-ax1.set_ylim((3,12.5))
-ax1.set_xlabel('J - K')
-ax1.set_ylabel('NUV - J')
-ax1.plot([-0.5,1.5],[3,12])
-a2 = ax1.scatter(newt['j_m'][ncut]-newt['k_m'][ncut],newt['nuv_mag'][ncut]-newt['j_m'][ncut],facecolor='none',edgecolor='red',s=30,alpha=0.1)
-a3 = ax1.scatter(pickles['j']-pickles['k'],pickles['nuv']-pickles['j'],c='black',s=5)
-#for j in range(len(pickles)):
-#    ax1.annotate(pickles['name'][j][:-4],xy=(pickles['j'][j]-pickles['k'][j],pickles['nuv'][j]-pickles['j'][j]),size=12)
-ax1.legend([a1,a2,a3],['GALEX sextractor','galex','pickles'],scatterpoints=1,loc=2)
 
 
-b1 = ax2.scatter(star.j[scut2]-star.k[scut2],star.nuv[scut2]-star.j[scut2],edgecolor='none',alpha=0.5)
-ax2.set_title('gb < '+str(gbrange)+', gl = '+str(i)+' to '+str(i+20))
-ax2.set_xlim((-0.5,1.5))
-ax2.set_ylim((3,12.5))
-ax2.set_xlabel('J - K')
-ax2.set_ylabel('NUV - J')
-ax2.plot([-0.5,1.5],[3,12])
-b2 = ax2.scatter(newt['j_m'][ncut2]-newt['k_m'][ncut2],newt['nuv_mag'][ncut2]-newt['j_m'][ncut2],facecolor='none',edgecolor='red',s=30,alpha=0.1)
+ax1.set_title('New Field, gb > '+str(gbrange))
+ax1.set_xlim((0,2))
+ax1.set_ylim((0,10))
+ax1.set_xlabel('B - V')
+ax1.set_ylabel('NUV - B')
+ax1.plot([0,2],[0,10])
+a2 = ax1.scatter(newt['BJmag'][ncut]-newt['VJmag'][ncut],newt['nuv_mag'][ncut]-newt['BJmag'][ncut],facecolor='none',edgecolor='red',s=30,alpha=0.3)
+a3 = ax1.scatter(pickles['b']-pickles['v'],pickles['nuv']-pickles['b'],c='black',s=5)
+for j in range(len(pickles)):
+    ax1.annotate(pickles['name'][j][:-4],xy=(pickles['b'][j]-pickles['v'][j],pickles['nuv'][j]-pickles['b'][j]),size=12)
 
-b3 = ax2.scatter(pickles['j']-pickles['k'],pickles['nuv']-pickles['j'],c='black',s=5)
-#for j in range(len(pickles)):
-#    ax2.annotate(pickles['name'][j][:-4],xy=(pickles['j'][j]-pickles['k'][j],pickles['nuv'][j]-pickles['j'][j]),size=12)
+a1 = ax1.scatter(star['BJmag'][scut]-star['VJmag'][scut],star['nuv'][scut]-star['BJmag'][scut],edgecolor='none',alpha=0.5)
+ax1.legend([a1,a2,a3],['Sextractor','galex','pickles'],scatterpoints=1,loc=2)
 
-ax2.legend([b1,b2,b3],['GALEX sextractor','galex','pickles'],scatterpoints=1,loc=2)
+
+ax2.set_title('New Field, gb < '+str(gbrange))
+ax2.set_xlim((0,2))
+ax2.set_ylim((0,10))
+ax2.set_xlabel('B - V')
+ax2.set_ylabel('NUV - B')
+ax2.plot([0,2],[0,10])
+b2 = ax2.scatter(newt['BJmag'][ncut2]-newt['VJmag'][ncut2],newt['nuv_mag'][ncut2]-newt['BJmag'][ncut2],facecolor='none',edgecolor='red',s=30,alpha=0.3)
+b3 = ax2.scatter(pickles['b']-pickles['v'],pickles['nuv']-pickles['b'],c='black',s=5)
+for j in range(len(pickles)):
+    ax2.annotate(pickles['name'][j][:-4],xy=(pickles['b'][j]-pickles['v'][j],pickles['nuv'][j]-pickles['b'][j]),size=12)
+
+b1 = ax2.scatter(star['BJmag'][scut2]-star['VJmag'][scut2],star['nuv'][scut2]-star['BJmag'][scut2],edgecolor='none',alpha=0.5)
+ax2.legend([b1,b2,b3],['Sextractor','galex','pickles'],scatterpoints=1,loc=2)
 plt.show()
     
 
@@ -146,4 +151,3 @@ ax2.set_ylabel('SExtractor - GALEX NUV')
 
 #ax2.legend([b1,b2,b3],['sextractor','galex','pickles'],scatterpoints=1,loc=2)
 plt.show()
-    
