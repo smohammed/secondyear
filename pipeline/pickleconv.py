8b631406e8f02280f00a4cc3ed49ff99982c2a09
Steven Mohammed <smohammed@astro.columbia.edu>
2015-08-20 16:06:39 -0400

    Fixed PICKLES SED points

--- pipeline/pickleconv.py
+++ pipeline/pickleconv.py
@@ -2,27 +2,110 @@ from astropy.io import ascii
 import numpy as np
 from astropy.table import Table
 
-pbands = Table.read('../picklesbands.txt', format='ascii')
-name = pbands['name']
-
-
 ##########################################################################
 # Add LePHARE fix
 ##########################################################################
-
 #to do:
 # 1. Figure out range for each band
 # 2. Make array for that range and pull data from Pickles
 # 3. Compute: (Sum PICKLE_lambda * LePHARE_lambda) / sum(LePHARE_lambda)
 # 4. Make table
 
-name,nuv,b,v,j,h,k = [],[],[],[],[],[],[]
+# Open LePHARE files
+directory = '../PICKLES/filt/'
+nuvfilt = Table.read(directory+'galex/NUV.pb', format='ascii')
+bfilt = Table.read(directory+'wfi/B.pb', format='ascii', data_start=1)
+vfilt = Table.read(directory+'wfi/V.pb', format='ascii', data_start=1)
+jfilt = Table.read(directory+'2mass/J.pb', format='ascii', data_start=1)
+hfilt = Table.read(directory+'2mass/H.pb', format='ascii', data_start=1)
+kfilt = Table.read(directory+'2mass/Ks.pb', format='ascii', data_start=1)
+
+
+# Get correct indices
+test = Table.read('../PICKLES/a0i.sed', format='ascii')
+nuv_starind, nuv_filtind = [], []
+b_starind, b_filtind = [], []
+v_starind, v_filtind = [], []
+j_starind, j_filtind = [], []
+h_starind, h_filtind = [], []
+k_starind, k_filtind = [], []
+inda, indn = [], []
+
+for i in range(len(test)):
+    for j in range(len(nuvfilt)):
+        if test['col1'][i] == nuvfilt['col1'][j]:
+            nuv_starind.append(i)
+            nuv_filtind.append(j)
+
+    for j in range(len(bfilt)):
+        if test['col1'][i] == bfilt['col1'][j]:
+            b_starind.append(i)
+            b_filtind.append(j)
+
+    for j in range(len(vfilt)):
+        if test['col1'][i] == vfilt['col1'][j]:
+            v_starind.append(i)
+            v_filtind.append(j)
+
+    for j in range(len(jfilt)):
+        if test['col1'][i] == jfilt['col1'][j]:
+            j_starind.append(i)
+            j_filtind.append(j)
+
+    for j in range(len(hfilt)):
+        if test['col1'][i] == hfilt['col1'][j]:
+            h_starind.append(i)
+            h_filtind.append(j)
+
+    for j in range(len(kfilt)):
+        if test['col1'][i] == kfilt['col1'][j]:
+            k_starind.append(i)
+            k_filtind.append(j)
+
+nuvfilt = nuvfilt[nuv_filtind]
+bfilt = bfilt[b_filtind]
+vfilt = vfilt[v_filtind]
+jfilt = jfilt[j_filtind]
+hfilt = hfilt[h_filtind]
+kfilt = kfilt[k_filtind]
+
+# Compute sums for denominator
+nuvfiltsum = np.sum(nuvfilt['col2'])
+bfiltsum = np.sum(bfilt['col2'])
+vfiltsum = np.sum(vfilt['col2'])
+jfiltsum = np.sum(jfilt['col2'])
+hfiltsum = np.sum(hfilt['col2'])
+kfiltsum = np.sum(kfilt['col2'])
+
+
+# Now compute numerator
+name, nuv, b, v, j, h, k = [], [], [], [], [], [], []
 picklefiles = np.loadtxt('../PICKLES/pickles.txt', dtype='string')
 
-
-'''
 for i in picklefiles:
-    a = Table.read('../PICKLES/'+i,format='ascii')
+    table = Table.read('../PICKLES/'+i, format='ascii')
+    name.append(i[:-4])
+    nuv.append(np.sum(table['col2'][nuv_starind] * nuvfilt['col2']))
+    b.append(np.sum(table['col2'][b_starind] * bfilt['col2']))
+    v.append(np.sum(table['col2'][v_starind] * vfilt['col2']))
+    j.append(np.sum(table['col2'][j_starind] * jfilt['col2']))
+    h.append(np.sum(table['col2'][h_starind] * hfilt['col2']))
+    k.append(np.sum(table['col2'][k_starind] * kfilt['col2']))
+
+# Now compute f_lambda
+nuv = [x/nuvfiltsum for x in nuv]
+b = [x/bfiltsum for x in b]
+v = [x/vfiltsum for x in v]
+j = [x/jfiltsum for x in j]
+h = [x/hfiltsum for x in h]
+k = [x/kfiltsum for x in k]
+
+# Old way
+'''
+name,nuv,b,v,j,h,k = [],[],[],[],[],[],[]
+picklefiles = np.loadtxt('PICKLES/pickles.txt', dtype='string')
+for i in picklefiles:
+    a = Table.read('PICKLES/'+i,format='ascii')
     name.append(i)
     nuv.append(a['col2'][224])
     b.append(a['col2'][641])
@@ -32,9 +115,7 @@ for i in picklefiles:
     k.append(a['col2'][4089])
 '''
 
-
-
-
+pbands = Table([nuv, b, v, j, k, k], names=('nuv', 'b', 'v', 'j', 'h', 'k'))
 
 ##########################################################################
 # convert f_lambda to f_v, divide by 0 point, then conv to Jy. 1 Jy = 10^-23 erg/s/Hz/cm^2
@@ -43,7 +124,6 @@ for i in picklefiles:
 #nuv = -2.5*np.log10((((pbands['nuv']-(2.22*10**-8))*3.34*10**4*(2300)**2))*10**-23)-48.6
 
 # Wavelengths in A
-
 #nuv = -2.5*np.log10(((pbands['nuv'] * 3.34*10**4 * (2300)**2)) * 10**-23) - 48.6
 #b = -2.5*np.log10(pbands['b'] * 3.34*10**4 * (4350)**2 * 10**-23) - 48.6
 #v = -2.5*np.log10(pbands['v'] * 3.34*10**4 * (5050)**2 * 10**-23) - 48.6
@@ -62,4 +142,4 @@ k = -2.5*np.log10(((pbands['k']*3.34*10**4 * (21590)**2))) + 7.060
 
 
 data = [name, nuv, b, v, j, h, k]
-ascii.write(data, 'picklemags.txt', names=['name', 'nuv', 'b', 'v', 'j', 'h', 'k'])
+ascii.write(data, '../picklemags_laphare.txt', names=['name', 'nuv', 'b', 'v', 'j', 'h', 'k'])
