33097a87734504a2c518ebe66a67cb9442420122
Steven Mohammed <smohammed@astro.columbia.edu>
2015-11-12 19:02:43 -0500

    Consolidated sky plot

--- pipeline/misc.py
+++ pipeline/misc.py
@@ -1,65 +1,4 @@
 ####################################################################
-# Made pickles SED dataset
-####################################################################
-name,nuv,b,v,j,h,k = [],[],[],[],[],[],[]
-picklefiles = np.loadtxt('PICKLES/pickles.txt', dtype='string')
-for i in picklefiles:
-    a = Table.read('PICKLES/'+i,format='ascii')
-    name.append(i)
-    nuv.append(a['col2'][224])
-    b.append(a['col2'][641])
-    v.append(a['col2'][781])
-    j.append(a['col2'][2241])
-    h.append(a['col2'][3095])
-    k.append(a['col2'][4089])
-
-####################################################################
-# NUV - B vs B - V plot
-####################################################################
-star = Table.read('newfield_2mass_t2_jlim_13.5_3arcsec.txt',format='ascii')
-newt = Table.read('galex0data_2mass_t2.txt', format='ascii')
-pickles = Table.read('picklemags.txt', format='ascii')
-gbrange = 5.
-scut = np.where((np.abs(star['gb_sex']) > gbrange))
-scut2 = np.where((np.abs(star['gb_sex']) < gbrange))
-ncut = np.where(np.abs(newt['gb_galex']) > gbrange)
-ncut2 = np.where(np.abs(newt['gb_galex']) < gbrange)
-
-f, (ax1, ax2) = plt.subplots(1, 2)
-plt.rc('legend',**{'fontsize':15})
-
-ax1.set_title('3", J < 13.5, Deeper Field, gb > '+str(gbrange))
-ax1.set_xlim((-0.5,2))
-ax1.set_ylim((2,10))
-ax1.set_xlabel('B - V')
-ax1.set_ylabel('NUV - B')
-ax1.plot([-0.5,2],[2,10])
-a2 = ax1.scatter(newt['BJmag'][ncut]-newt['VJmag'][ncut],newt['nuv_mag'][ncut]-newt['BJmag'][ncut],facecolor='none',edgecolor='red',s=30,alpha=0.3)
-a3 = ax1.scatter(pickles['b']-pickles['v'],pickles['nuv']-pickles['b'],c='black',s=5)
-for j in range(len(pickles)):
-    ax1.annotate(pickles['name'][j][:-4],xy=(pickles['b'][j]-pickles['v'][j],pickles['nuv'][j]-pickles['b'][j]),size=12)
-
-a1 = ax1.scatter(star['BJmag'][scut]-star['VJmag'][scut],star['nuv'][scut]-star['BJmag'][scut],edgecolor='none',alpha=0.5)
-ax1.legend([a1,a2,a3],['Sextractor','GALEX','Pickles'],scatterpoints=1,loc=4)
-
-ax2.set_title('3", J < 13.5, Deeper Field, gb < '+str(gbrange))
-ax2.set_xlim((-0.5,2))
-ax2.set_ylim((2,10))
-ax2.set_xlabel('B - V')
-ax2.set_ylabel('NUV - B')
-ax2.plot([-0.5,2],[2,10])
-b2 = ax2.scatter(newt['BJmag'][ncut2]-newt['VJmag'][ncut2],newt['nuv_mag'][ncut2]-newt['BJmag'][ncut2],facecolor='none',edgecolor='red',s=30,alpha=0.3)
-b3 = ax2.scatter(pickles['b']-pickles['v'],pickles['nuv']-pickles['b'],c='black',s=5)
-for j in range(len(pickles)):
-    ax2.annotate(pickles['name'][j][:-4],xy=(pickles['b'][j]-pickles['v'][j],pickles['nuv'][j]-pickles['b'][j]),size=12)
-
-b1 = ax2.scatter(star['BJmag'][scut2]-star['VJmag'][scut2],star['nuv'][scut2]-star['BJmag'][scut2],edgecolor='none',alpha=0.5)
-ax2.legend([b1,b2,b3],['Sextractor','GALEX','Pickles'],scatterpoints=1,loc=4)
-plt.show()
-
-    
-
-####################################################################
 # NUV - J vs J - K
 ####################################################################
 star = Table.read('newfield_2mass_t2_jlim_13.5_3arcsec.txt',format='ascii')
@@ -263,62 +202,6 @@ star.rename_column('k_m','k')
 
 
 ####################################################################
-# NUV comparison between GALEX and SExtractor
-####################################################################
-star = Table.read('newfield_2mass_t2_jlim_13.5_3arcsec.txt',format='ascii')
-galex = Table.read('galex0data_2mass_t2.txt',format='ascii')
-pickles = Table.read('picklemags.txt',format='ascii')
-
-stargal = SkyCoord(star.gl*u.deg,star.gb*u.deg,frame='galactic')
-galexgal = SkyCoord(galex['gl_galex']*u.deg,galex['gb_galex']*u.deg,frame='galactic')
-starind, galexind, angsep, dist3d = search_around_sky(stargal, galexgal, 6.*u.arcsec)
-star2 = star[starind]
-galex2 = galex[galexind]
-
-gbrange = 5.
-scut = np.where((np.abs(star['gb_sex']) > gbrange))
-scut2 = np.where((np.abs(star['gb_sex']) < gbrange))
-ncut = np.where(np.abs(newt['gb_galex']) > gbrange)
-ncut2 = np.where(np.abs(newt['gb_galex']) < gbrange)
-f, (ax1, ax2) = plt.subplots(1, 2)
-plt.rc('legend',**{'fontsize':15})
-a1 = ax1.scatter(galex2['nuv_mag'][ncut],star2['nuv'][scut]-galex2['nuv_mag'][ncut],edgecolor='none',alpha=0.2)
-ax1.set_title('gb > '+str(gbrange)+', gl = '+str(i)+' to '+str(i+20))
-ax1.set_xlim((13,20))
-ax1.set_ylim((-2,3))
-ax1.set_xlabel('GALEX NUV')
-ax1.set_ylabel('SExtractor - GALEX NUV')
-
-
-a2 = ax2.scatter(galex2['nuv_mag'][ncut2],star2['nuv'][scut2]-galex2['nuv_mag'][ncut2],edgecolor='none',alpha=0.2)
-ax2.set_title('gb < '+str(gbrange)+', gl = '+str(i)+' to '+str(i+20))
-ax2.set_xlim((13,20))
-ax2.set_ylim((-2,3))
-ax2.set_xlabel('GALEX NUV')
-ax2.set_ylabel('SExtractor - GALEX NUV')
-plt.show()
-
-
-plt.scatter(g2['nuv_mag'],s2['nuv']-1,g2['nuv_mag'],edgecolor='none',alpha=0.3)
-plt.plot([12,20],[12,20],c='black')
-plt.xlabel('SExtractor NUV')
-plt.ylabel('GALEX NUV')
-plt.title('GALEX0 vs SExtractor NUV')
-plt.xlim((12,20))
-plt.ylim((12,20))
-plt.show()
-
-
-plt.scatter(g2['nuv_mag'],s2['nuv']-g2['nuv_mag'],edgecolor='none',alpha=0.3)
-plt.xlabel('GALEX NUV')
-plt.ylabel('SExtractor - GALEX NUV')
-plt.title('NUV Comparison')
-plt.xlim((13,21))
-plt.ylim((-2,2))
-plt.plot([13,21],[1,1],c='black')
-plt.show()
-
-####################################################################
 # Plot data w/ J limits
 ####################################################################
 staroutcut = np.where(((star['j']-star['k']) >  0.6) & ((star['nuv']-star['j']) < 8))
@@ -394,19 +277,6 @@ plt.title('2MASS match counts by J_lim and radius search')
 plt.legend(scatterpoints=1,loc=2)
 plt.show()
 
-
-# NUV vs J
-for i in files:
-    plt.scatter(i['j'],i['nuv'],edgecolor='none',alpha=0.2)
-    plt.show()
-
-x = 0
-for i in files:
-    lim = np.ones(len(i))*jlim[x]
-    print jlim[x]
-    plt.scatter(lim,i['dist_x'],edgecolor='none',alpha=0.01)
-    x+=1
-
 ####################################################################
 # Add WCS coords to plots
 ####################################################################
@@ -464,17 +334,6 @@ s2m2 = sex2mass[sex2mind]
 vpm2 = vphas[vp2mind]
 
 s2mv = hstack([s2m2,vpm2])
-
-
-
-'''
-sex = Sextractor
-sv = sextractor + vphas
-sex2mass = sextractor + 2MASS
-s2mv = sextractor + 2mass + vphas
-'''
-
-
 
 plt.scatter(sv['g_AB']-sv['i_AB'],sv['nuv']-sv['g_AB'],edgecolor='none',alpha=0.3,label='VPHAS')
 plt.scatter(vs2m['g_AB']-vs2m['i_AB'],vs2m['nuv']-vs2m['g_AB'],edgecolor='red',facecolor='none',alpha=0.3,label='VPHAS+2MASS')
@@ -577,19 +436,6 @@ f.subplots_adjust(wspace=0)
 plt.show()
 
 
-
-
-
-plt.hist(sv['nuv'],bins=bins,label='VPHAS+SExtractor',color='green')
-plt.hist(sex2mass['nuv'][s2masscut],bins=bins,label='2MASS+SExtractor',alpha=0.7,color='red')
-plt.title('VPHAS vs 2MASS for abs(gb) < 4')
-plt.xlabel('NUV')
-plt.legend(loc=2)
-plt.show()
-
-
-
-
 ####################################################################
 # Comparing 
 ####################################################################
@@ -650,127 +496,7 @@ plt.arrow(-1.5, 4,g[3]-ib[3], nuv[3]-g[3], head_length=0.1,head_width=0.07,color
 
 for j in range(0,len(pickles),10):
         plt.annotate(pickles['name'][j], xy=((pickles['g'][j]-pickles['i'][j])+0.01, (pickles['nuv'][j]-pickles['g'][j]) - 0.07), size=15)
-
-
 plt.show()
-
-
-#######################################################
-# g - r vs NUV - g
-#######################################################
-
-plt.scatter(sexv['nuv']-sexv['g_AB'],sexv['g_AB']-sexv['r_AB'],edgecolor='none',alpha=0.3,label='VPHAS')
-#plt.scatter(starv['nuv']-starv['g_AB'],starv['g_AB']-starv['r_AB'],edgecolor='none',facecolor='red',alpha=0.3,label='VPHAS+2MASS')
-plt.xlabel('NUV - g (ABmag)')
-plt.ylabel('g - r (ABmag)')
-plt.xlim((-3,9))
-plt.ylim((-1,2))
-plt.scatter(pickles['nuv']-pickles['g'], pickles['g']-pickles['r'], c='black', s=10)
-
-plt.arrow(4, -0.75,nuv[3]-g[3], g[3]-r[3], head_length=0.1,head_width=0.07,color='black')
-
-#plt.legend(scatterpoints=1,loc=2)
-
-#for j in range(0,len(pickles),10):
-#        plt.annotate(pickles['name'][j], xy=((pickles['nuv'][j]-pickles['g'][j])+0.01, (pickles['g'][j]-pickles['r'][j]) - 0.07), size=15)
-plt.show()
-
-#######################################################
-# NUV - g vs g - i
-#######################################################
-plt.scatter(sexv['g_AB']-sexv['i_AB'],sexv['nuv']-sexv['g_AB'],edgecolor='none',facecolor='blue',alpha=0.3,label='VPHAS')
-plt.scatter(starv['g_AB']-starv['i_AB'],starv['nuv']-starv['g_AB'],edgecolor='none',facecolor='red',label='VPHAS + 2MASS',alpha=0.3)
-plt.xlim((-2,3))
-plt.ylim((-2,8))
-plt.xlabel('g - i (ABmag)')
-plt.ylabel('NUV - g (ABmag)')
-plt.legend(loc=3,scatterpoints=1)
-plt.gca().invert_yaxis()
-plt.scatter(pickles['g']-pickles['i'], pickles['nuv']-pickles['g'], c='black', s=10)
-
-plt.arrow(-1.5, 4,g[3]-ib[3], nuv[3]-g[3], head_length=0.1,head_width=0.07,color='black')
-
-for j in range(0,len(pickles),10):
-        plt.annotate(pickles['name'][j], xy=((pickles['g'][j]-pickles['i'][j])+0.01, (pickles['nuv'][j]-pickles['g'][j]) - 0.07), size=15)
-plt.show()
-
-
-#######################################################
-# g - r vs u - g
-#######################################################
-
-plt.scatter(sexv['u_AB']-sexv['g_AB'],sexv['g_AB']-sexv['r_AB'],edgecolor='none',alpha=0.3,label='VPHAS',color='blue')
-#plt.scatter(starv['u_AB']-starv['g_AB'],starv['g_AB']-starv['r_AB'],edgecolor='none',facecolor='red',alpha=0.3,label='VPHAS+2MASS')
-
-cut = np.where(sexv['nuv'] - sexv['g_AB'] < 3)
-plt.scatter(sexv[cut]['u_AB']-sexv[cut]['g_AB'],sexv[cut]['g_AB']-sexv[cut]['r_AB'],edgecolor='none',facecolor='violet',alpha=0.3,label='NUV - g < 3')
-
-plt.xlabel('u - g (ABmag)')
-plt.ylabel('g - r (ABmag)')
-plt.xlim((-1,4))
-plt.ylim((-1,2))
-plt.scatter(pickles['u']-pickles['g'], pickles['g']-pickles['r'], c='black', s=10)
-
-plt.arrow(2, -0.5,u[3]-g[3], g[3]-r[3], head_length=0.1,head_width=0.07,color='black')
-
-plt.legend(scatterpoints=1,loc=2)
-
-plt.annotate('White Dwarfs',xy=(-0.95,0))
-plt.annotate('M+WD Binaries',xy=(1.5,1.5))
-for j in range(0,len(pickles),10):
-        plt.annotate(pickles['name'][j], xy=((pickles['u'][j]-pickles['g'][j])+0.01, (pickles['g'][j]-pickles['r'][j]) - 0.07), size=15)
-plt.show()
-
-
-#######################################################
-# NUV - J vs J - K
-#######################################################
-plt.scatter(starv['j']-starv['k'],starv['nuv']-starv['j'],edgecolor='none',alpha=0.3,label='2MASS+VPHAS')
-plt.scatter(starv['j'][a]-starv['k'][a],starv['nuv'][a]-starv['j'][a],edgecolor='none',facecolor='red',label='NUV - g < 3')
-plt.xlim((-0.25, 2.5))
-plt.ylim((0, 14))
-plt.ylabel('NUV - J')
-plt.xlabel('J - K')
-plt.legend(scatterpoints=1)
-plt.scatter(pickles['j']-pickles['k'], pickles['nuv']-pickles['j'], c='black', s=10)
-plt.axhline(y=4,clor='black')
-plt.arrow(-0.1, 8, 0.2876-0.1170, 2.9720-0.2876, head_length=1.,head_width=0.07,color='black')
-plt.show()
-
-# GALEX, Pickles, SExtractor order
-nuv = ['nuv_mag', 'nuv', 'nuv', 2.9720]
-b = ['BJmag', 'b', 'BJmag', 1.3429]
-v = ['VJmag', 'v', 'VJmag', 1.0]
-j = ['j', 'j', 'j', 0.2876]
-h = ['h', 'h', 'h', 0.1783]
-k = ['k', 'k', 'k', 0.1170]
-u = ['u','u_AB','u_AB',1.5916]
-g = ['g','g_AB','g_AB',1.1838]
-r = ['r','r_AB','r_AB',0.8664]
-ib  = ['i','i_AB','i_AB',0.6418]
-
-if nuvjvsjk == 1:
-    x1 = j
-    x2 = k
-    y1 = nuv
-    y2 = j
-    extx = -0.1
-    exty = 8
-    #extx = 0.08
-    #exty = 3.5
-    extdx = x1[3] - x2[3]
-    extdy = y1[3] - y2[3]
-
-
-
-cut = np.where(((sexv['g_AB'] - sexv['r_AB']) > 0.6) & ((sexv['g_AB'] - sexv['r_AB']) < 1.5) & ((sexv['nuv'] - sexv['g_AB']) > 0) & ((sexv['nuv'] - sexv['g_AB']) < 2))
-
-cutstar = np.where(((starv['g_AB'] - starv['r_AB']) > 0.6) & ((starv['g_AB'] - starv['r_AB']) < 1.5) & ((starv['nuv'] - starv['g_AB']) > 0) & ((starv['nuv'] - starv['g_AB']) < 2))
-
-
-cutstar = np.where(((starv['u_AB'] - starv['g_AB']) > -1) & ((starv['u_AB'] - starv['g_AB']) < 0.5))
-
-g2['ra_2mass'][(min(g2['ra_2mass']) < 10.) & (g2['ra_2mass'] > 350.)] = g2['ra_2mass'][(min(g2['ra_2mass']) < 10.) & (g2['ra_2mass'] > 350.)] - 360.
 
 
 delang = 2*np.arcsin(np.sqrt(np.sin((star['dec_sex']-star['dec_2mass'])/2)**2+np.cos(star['dec_sex'])*np.cos(star['dec_2mass'])*np.sin((star['ra_sex']-star['ra_2mass'])/2)**2))
@@ -885,9 +611,10 @@ galexind,vpind,angsep,ang3d = search_around_sky(galexgal,vpgal,3*u.arcsec)
 
 g3 = g2[galexind]
 
-plt.scatter(g2['glon'],g2['glat'],edgecolor='none')
-plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red')
-plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow')
+plt.scatter(g2['glon'],g2['glat'],edgecolor='none',label='galex')
+plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red',label='vphas')
+plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow',label='both')
+plt.legend(scatterpoints=1)
 plt.show()
 
 
@@ -899,11 +626,11 @@ vp = fits.open('vphas_allg_gl200-250.fits')[1].data
 vpgal = SkyCoord(vp['RAJ2000']*u.deg,vp['DEJ2000']*u.deg,frame='icrs').galactic
 galexind,vpind,angsep,ang3d = search_around_sky(galexgal,vpgal,3*u.arcsec)
 g3 = g2[galexind]
-plt.scatter(g2['glon'],g2['glat'],edgecolor='none')
-plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red')
-plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow')
+plt.scatter(g2['glon'],g2['glat'],edgecolor='none',label='galex')
+plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red',label='vphas')
+plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow',label='both')
+plt.legend(scatterpoints=1)
 plt.show()
-
 
 galexcut = np.where((galex['glon'] > 250) & (galex['glon'] < 300))
 g2 = galex[galexcut]
@@ -912,11 +639,11 @@ vp = fits.open('vphas_allg_gl250-300.fits')[1].data
 vpgal = SkyCoord(vp['RAJ2000']*u.deg,vp['DEJ2000']*u.deg,frame='icrs').galactic
 galexind,vpind,angsep,ang3d = search_around_sky(galexgal,vpgal,3*u.arcsec)
 g3 = g2[galexind]
-plt.scatter(g2['glon'],g2['glat'],edgecolor='none')
-plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red')
-plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow')
+plt.scatter(g2['glon'],g2['glat'],edgecolor='none',label='galex')
+plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red',label='vphas')
+plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow',label='both')
+plt.legend(scatterpoints=1)
 plt.show()
-
 
 galexcut = np.where((galex['glon'] > 300) & (galex['glon'] < 360))
 g2 = galex[galexcut]
@@ -925,16 +652,40 @@ vp = fits.open('vphas_allg_gl300-360.fits')[1].data
 vpgal = SkyCoord(vp['RAJ2000']*u.deg,vp['DEJ2000']*u.deg,frame='icrs').galactic
 galexind,vpind,angsep,ang3d = search_around_sky(galexgal,vpgal,3*u.arcsec)
 g3 = g2[galexind]
-plt.scatter(g2['glon'],g2['glat'],edgecolor='none')
-plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red')
-plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow')
+plt.scatter(g2['glon'],g2['glat'],edgecolor='none',label='galex')
+plt.scatter(vpgal.l.degree,vpgal.b.degree,edgecolor='none',facecolor='red',label='vphas')
+plt.scatter(g3['glon'],g3['glat'],edgecolor='none',facecolor='yellow',label='both')
+plt.legend(scatterpoints=1)
 plt.show()
 
 
-# for GALEX - count up circles, * by diamater of aperture
-# For VPHAS - 
+fig,axes = plt.subplots(2,2,sharey=True)
 
-# 0-40: Basically same as paper
-# 200-250: 
-# 250-300:
-# 300-360: 
+axes[0,0].scatter(gl,gb,label='All WDs')
+axes[0,1].scatter(gl,gb)
+axes[1,1].scatter(gl,gb)
+axes[1,0].scatter(gl,gb)
+axes[0,0].scatter(gl[cut],gb[cut],c='red',label='NUV-g > 3')
+axes[0,1].scatter(gl[cut],gb[cut],c='red')
+axes[1,1].scatter(gl[cut],gb[cut],c='red')
+axes[1,0].scatter(gl[cut],gb[cut],c='red')
+
+axes[0,0].set_xlim(-2,40)
+axes[0,1].set_xlim(205,252)
+axes[1,0].set_xlim(252,300)
+axes[1,1].set_xlim(300,362)
+axes[0,0].set_ylim(-6,6)
+axes[0,1].set_ylim(-6,6)
+axes[1,0].set_ylim(-6,6)
+axes[1,1].set_ylim(-6,6)
+axes[1,0].set_xlabel('gl')
+axes[1,1].set_xlabel('gl')
+axes[1,0].set_ylabel('gb')
+axes[0,0].set_ylabel('gb')
+axes[0,0].legend(scatterpoints=1)
+fig.subplots_adjust(wspace=0)
+plt.show()
+
+
+
+
