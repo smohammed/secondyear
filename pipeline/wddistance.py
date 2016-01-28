b6fdd7de7be3c9bf2e50aba9cfe03fd5f2895d69
Steven Mohammed <smohammed@astro.columbia.edu>
2016-01-28 17:03:02 -0500

    Fixed heat map plots

--- pipeline/wddistance.py
+++ pipeline/wddistance.py
@@ -13,7 +13,7 @@ wd = wd[wdcut]
 
 # Input constants, set Teff and Radius
 sigma = 5.6704 * 10**-5             # erg/cm^2/s/K^4
-radius = 637.1 * 10**6/2.              # cm
+radius = 637.1 * 10**6              # cm
 Temp = np.arange(10, 31, 5)*10**3   # K
 Lsun = 3.828 * 10**33               # erg/s
 #Lstar = 3.0128 * 10**35             # erg/s, bolometric abs mag
@@ -50,7 +50,7 @@ dust = query(gl, gb, coordsys='gal')
 DM = dust['distmod']
 
 # Table will be len(comptable) * len(DM) * len(Mabs) long
-table = Table(np.empty((0, 19)))
+table = Table(np.empty((0, 20)))
 
 for i in range(len(comptable)):
     for j in range(len(DM)):
@@ -59,18 +59,19 @@ for i in range(len(comptable)):
             distmod = DM[j]
             tem = Temp[k]
             # Calculate apparent magnitude model
-            mmodel_nuv = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_nuv + distmod + EBV
-            mmodel_u = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_u + distmod + EBV
-            mmodel_g = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_g + distmod + EBV
-            mmodel_r = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_r + distmod + EBV
-            mmodel_i = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_i + distmod + EBV
+            mmodel_nuv = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_nuv + distmod + EBV * 7.76/3.1
+            mmodel_u = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_u + distmod + EBV * 4.239/3.1
+            mmodel_g = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_g + distmod + EBV * 3.303/3.1
+            mmodel_r = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_r + distmod + EBV * 2.285/3.1
+            mmodel_i = -2.5*np.log10(sigma * tem**4 * 4 * np.pi * radius**2 / Lsun) + Msun_i + distmod + EBV * 1.698/3.1
             # Compute chi^2
             chi2nuv = (comptable['nuv_mag'][i] - mmodel_nuv)**2
             chi2u = (comptable['u_AB'][i] - mmodel_u)**2
             chi2g = (comptable['g_AB'][i] - mmodel_g)**2
             chi2r = (comptable['r_AB'][i] - mmodel_r)**2
             chi2i = (comptable['i_AB'][i] - mmodel_i)**2
-            a = [i, comptable['nuv_mag'][i], comptable['u_AB'][i], comptable['g_AB'][i], comptable['r_AB'][i], comptable['i_AB'][i], distmod, tem, EBV, mmodel_nuv, mmodel_u, mmodel_g, mmodel_r, mmodel_i, chi2nuv, chi2u, chi2g, chi2r, chi2i]
+            chi2 = (chi2nuv + chi2u + chi2g + chi2r + chi2i)/5.
+            a = [i, comptable['nuv_mag'][i], comptable['u_AB'][i], comptable['g_AB'][i], comptable['r_AB'][i], comptable['i_AB'][i], distmod, tem, EBV, mmodel_nuv, mmodel_u, mmodel_g, mmodel_r, mmodel_i, chi2nuv, chi2u, chi2g, chi2r, chi2i, chi2]
             table.add_row(a)
 
 table.rename_column('col0', 'wdnum')
@@ -92,9 +93,9 @@ table.rename_column('col15', 'chi2_u')
 table.rename_column('col16', 'chi2_g')
 table.rename_column('col17', 'chi2_r')
 table.rename_column('col18', 'chi2_i')
+table.rename_column('col19', 'chi2_avg')
 
-ascii.write(table, 'wd_GV_model_0.5ER_Msuncalc.txt', format='basic')
-
+ascii.write(table, 'wd_GV_model_1ER_Msuncalc.txt', format='basic')
 
 
 # Find minimum chi^2 value and only plot those
@@ -114,7 +115,7 @@ chimap[np.isnan(chimap)] = 0
 for i in range(len(DM)):
     for j in range(len(temp)):
         chicut = np.where((DM[i] == newtable['DM']) & (temp[j] == newtable['Temp']))
-        chimap[i][j] = chimap[i][j] + sum(newtable['chi2_nuv'][chicut])
+        chimap[i][j] = chimap[i][j] + sum(newtable['chi2_i'][chicut])
         #chimap[i][j] = len(newtable['chi2'][chicut])
 
 yv, xv = np.meshgrid(temp, DM)
@@ -122,10 +123,11 @@ plt.pcolormesh(xv, yv, chimap/len(newtable), vmin=0, vmax=1)
 plt.xlabel('Distance Modulus')
 plt.ylabel('Temperature [K]')
 cm = plt.colorbar()
-cm.set_label('$\chi^2$ (NUV$_{obs}$ - NUV$_{mod}$)$^2$')
+#cm.set_label('$\chi^2/$n$_{bands}$ (m$_{obs}$ - m$_{mod}$)$^2$')
+cm.set_label('$\chi^2$ (i$_{obs}$ - i$_{mod}$)$^2$')
 #cm.set_label('n$_{WDs}$')
-plt.title('GV WD model, 3D dust map, NUV AB, 1 R$_{Earth}$')
-plt.xlim((4, 18))
+plt.title('GV WD model, 3D dust map, i, 1 R$_{Earth}$')
+plt.xlim((4, 19))
 plt.show()
 
 
@@ -136,7 +138,7 @@ for i in range(0, len(table), 31*5):
 table = table[chiind]
 
 
-plt.hist2d(table['DM'],table['Temp'])
+plt.hist2d(table['DM'], table['Temp'])
 plt.xlabel('Distance Modulus')
 plt.ylabel('Temperature [K]')
 cm = plt.colorbar()
