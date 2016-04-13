c459de0201636455b71916d59176f69e710d20db
Steven Mohammed <smohammed@astro.columbia.edu>
2016-04-13 16:10:25 -0400

    New NUV comp, pos plots added

--- pipeline/misc.py
+++ pipeline/misc.py
@@ -955,3 +955,123 @@ plt.scatter(b['gl'], b['gb'], c='red', edgecolor='none')
 plt.scatter(a2['gl'], a2['gb'], color='green', alpha=0.5)
 plt.scatter(b2['gl'], b2['gb'], color='orange', alpha=0.5)
 
+
+
+s1 = ['1.4', '2.3', '3.2', '4.1', '5.0', '5.9', '6.8', '8.6', '9.5', '10.4', '11.3', '12.2', '14.0', '14.9', '15.8', '16.7', '17.6', '18.5', '19.4', '20.3', '21.2', '22.1', '23.0', '23.9', '24.8', '25.7', '28.4', '29.3', '30.2', '31.1', '32.0', '32.9', '33.8', '34.7', '35.6', '39.2', '42.8', '43.7', '44.6', '45.5', '46.4', '47.3', '48.2', '49.1', '50.0', '67.1', '68.9', '71.6', '74.3', '75.2', '76.1', '77.0', '77.9', '78.8', '79.7', '80.6', '81.5', '82.4', '83.3', '87.8', '88.7', '89.6', '90.5', '91.4', '92.3', '93.2', '94.1', '95.0', '95.9', '96.8', '97.7', '98.6', '99.5', '100.4', '101.3', '110.3', '102.2', '103.1', '104.0', '104.9', '105.8', '106.7', '107.6', '111.2', '112.1', '113.0', '113.9', '114.8', '119.3', '121.1', '122.9', '124.7', '125.6', '126.5', '127.4', '128.3', '129.2', '130.1', '131.0', '131.9', '132.8', '133.7', '134.6', '135.5', '136.4', '137.3', '138.2', '139.1', '140.0', '140.9', '141.8', '143.6', '144.5', '148.1', '149.0', '149.9', '150.8', '151.7', '152.6', '153.5', '145.4', '155.3', '156.2', '157.1', '158.0', '160.7', '161.6', '163.4', '167.0', '167.9', '172.4', '173.3', '174.2', '175.1', '176.0', '176.9', '177.8', '178.7', '179.6', '180.5', '183.2', '185.0', '190.4', '191.3', '197.6', '198.5', '200.3', '201.2', '203.0', '203.9', '205.7', '206.6', '207.5', '208.4', '209.3', '210.2', '211.1', '212.0', '212.9', '213.8', '214.7', '215.6', '216.5', '217.4', '218.3', '219.2', '220.1', '221.0', '221.9', '222.8', '223.7', '224.6', '225.5', '226.4', '228.2', '229.1', '230.0', '230.9', '231.8', '234.5', '235.4', '236.3', '237.2', '238.1', '239.0', '239.9', '240.8', '241.7', '242.6', '243.5', '244.4', '245.3', '246.2', '247.1', '248.0', '248.9', '249.8', '250.7', '251.6', '252.5', '253.4', '254.3', '255.2', '256.1', '257.0', '258.8', '259.7', '260.6', '261.5', '263.3', '264.2', '265.1', '266.0', '266.9', '268.7', '269.6', '270.5', '271.4', '272.3', '273.2', '274.1', '275.0', '275.9', '276.8', '278.6', '279.5', '281.3', '283.1', '284.0', '285.8', '286.7', '288.5', '289.4', '290.3', '291.2', '292.1', '293.0', '293.9', '295.7', '297.5', '298.4', '301.1', '302.0', '302.9', '303.8', '304.7', '305.6', '306.5', '308.3', '309.2', '310.1', '315.5', '316.4', '317.3', '318.2', '319.1', '320.0', '320.9', '321.8', '322.7', '323.6', '324.5', '325.4', '326.3', '327.2', '328.1', '329.0', '329.9', '331.7', '332.6', '333.5', '334.4', '335.3', '338.0', '338.9', '339.8', '341.6', '342.5', '343.4', '345.2', '348.8', '349.7', '350.6', '351.5', '352.4', '353.3', '354.2', '355.1', '356.0', '357.8', '358.7', '359.6']
+
+table = Table.read('starcat_200.3mapweight.txt', format='ascii')
+
+for region in s3:
+    print region
+    a = Table.read('starcat_'+region+'mapweight.txt',format='ascii')
+    table = vstack((table, a))
+
+ascii.write(table, 'starcat_set1-10.txt',format='basic')
+
+
+galex = fits.open('../GALEXAIS.fits')[1].data
+galexgal = SkyCoord(galex['glon']*u.deg, galex['glat']*u.deg, frame='galactic')
+filt = 'mexhat_4.0_9x9'
+code = 'det_thresh2.5_phot_autopar2.5_3.5'
+
+fits.HDUList([fits.PrimaryHDU(table)]).writeto('sex_galex_total.fits')
+
+
+for region in t:
+    cat = Table.read('Dunmaps/'+region,format='ascii')
+    sexgal = SkyCoord(cat['gl']*u.deg, cat['gb']*u.deg, frame='galactic')
+    sexind,  galexind,  angsep,  ang3d = search_around_sky(sexgal, galexgal, 3.5*u.arcsec)
+    comb = hstack([cat[sexind],Table(galex[galexind])])
+    comb['angsep'] = angsep
+    comb.rename_column('nuv','nuv_sex')
+    comb.rename_column('nuv_mag','nuv_galex')
+    comb.rename_column('gl','gl_sex')
+    comb.rename_column('gb','gb_sex')
+    comb.rename_column('glon','gl_galex')
+    comb.rename_column('glat','gb_galex')
+    comb.rename_column('ra_1','ra_sex')
+    comb.rename_column('dec_1','dec_sex')
+    comb.rename_column('ra_2','ra_galex')
+    comb.rename_column('dec_2','dec_galex')
+
+    ascii.write(comb,'sex_galex_matches_'+region+'.txt',format='basic')
+
+
+
+cat = Table.read('sex_tycho_matches_total_mapweight.txt', format='ascii')
+c1 = cat[np.where(cat['gb_tycho'] < 0)]
+c2 = cat[np.where(cat['gb_tycho'] > 0)]
+
+for region in range(0,360,5):
+    cat1 = c1[np.where((c1['gl_tycho'] > region) & (c1['gl_tycho'] < region+5))]
+    dgl1 = (cat1['gl_sex']-cat1['gl_tycho'])
+    dgb1 = (cat1['gb_sex']-cat1['gb_tycho'])
+
+    cat2 = c2[np.where((c2['gl_tycho'] > region) & (c2['gl_tycho'] < region+5))]
+    dgl2 = (cat2['gl_sex']-cat2['gl_tycho'])
+    dgb2 = (cat2['gb_sex']-cat2['gb_tycho'])
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+    ax1.scatter(dgl1*3600, dgb1*3600, edgecolor='none', alpha=0.1)
+    ax1.axhline(y=0,c='black')
+    ax1.axvline(x=0,c='black')
+    ax2.scatter(dgl2*3600, dgb2*3600, edgecolor='none', alpha=0.1)
+    ax2.axhline(y=0,c='black')
+    ax2.axvline(x=0,c='black')
+
+    ax1.set_xlabel('gl$_{SEx}$ - gl$_{Tycho2}$ [arcsec]')
+    ax2.set_xlabel('gl$_{SEx}$ - gl$_{Tycho2}$ [arcsec]')
+    ax1.set_ylabel('gb$_{SEx}$ - gb$_{Tycho2}$ [arcsec]')
+    ax1.set_xlim((-4,4))
+    ax1.set_ylim((-4,4))
+    ax2.set_xlim((-4,4))
+    ax2.set_ylim((-4,4))
+    ax1.annotate('N$_{lower}$ = '+str(len(cat1)), xy=(-3.9,-3.9))
+    ax2.annotate('N$_{upper}$ = '+str(len(cat2)), xy=(-3.9,-3.9))
+    ax1.set_title('gb<0, gl='+str(region)+'-'+str(region+5)+', avg=('+str(np.mean(dgl1)*3600)[:4]+', '+str(np.mean(dgb1)*3600)[:4]+')')
+    ax2.set_title('gb>0, avg=('+str(np.mean(dgl2)*3600)[:4]+', '+str(np.mean(dgb2)*3600)[:4]+')')
+    fig.subplots_adjust(wspace=0)
+
+    plt.savefig('04-13-sex-tycho_offset_'+str(region)+'-'+str(region+5)+'.png')
+    plt.clf()
+    print region
+
+
+img = fits.open('../Dunmaps/count_map_name_'+region+'_gal_sec_in.fits')[0].data
+im1xmin, im1xmax, im1ymin, im1ymax = 1214, 3950, 3532, 51230
+img = img[im1ymin:im1ymax, im1xmin:im1xmax]
+
+plt.imshow(img,vmin=0,vmax=1,origin='lower',interpolation='nearest',aspect='auto',cmap=cm.gray,extent=[x0,xn,y0,yn])
+
+
+
+agal = SkyCoord(a['gl']*u.deg,a['gb']*u.deg, frame='galactic')
+aind,  gaind,  angsepa,  ang3d = search_around_sky(agal, galexgal, 3.5*u.arcsec)
+a2 = Table(a[aind])
+g2 = Table(galex[gaind])
+comb = hstack([a2, g2])
+comb['angsep'] = angsepa
+comb.rename_column('nuv', 'nuv_sex')
+comb.rename_column('nuv_mag', 'nuv_galex')
+comb.rename_column('gl', 'gl_sex')
+comb.rename_column('gb', 'gb_sex')
+comb.rename_column('ra_1', 'ra_sex')
+comb.rename_column('dec_1', 'dec_sex')
+comb.rename_column('ra_2', 'ra_galex')
+comb.rename_column('dec_2', 'dec_galex')
+comb.rename_column('glon', 'gl_galex')
+comb.rename_column('glat', 'gb_galex')
+combcut = np.where(comb['nuv_galex'] == -999.)
+comb.remove_rows(combcut)
+
+plt.scatter(comb['nuv_galex'], comb['nuv_sex']-comb['nuv_galex'], alpha=0.1, edgecolor='none')
+plt.axhline(y=0, c='green')
+plt.xlabel('NUV$_{GAIS}$')
+plt.ylabel('NUV$_{SEx}$ - NUV$_{GAIS}$')
+plt.title('gl = 329.0')
+plt.xlim((12, 23))
+plt.ylim((-3, 2))
+plt.xlim((12, 23))
+plt.ylim((-3, 2))
+plt.annotate('N = '+str(len(comb)), xy=(13, -2.5))
+plt.show()
